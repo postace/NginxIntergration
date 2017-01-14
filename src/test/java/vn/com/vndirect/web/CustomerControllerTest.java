6136927d5270d59c1defb141f578b4bbe4c1b67f
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-01-14 11:27:47 +0700

    adding viewResolver class to force Spring doesn't check circular view paths

--- src/test/java/vn/com/vndirect/web/CustomerControllerTest.java
+++ src/test/java/vn/com/vndirect/web/CustomerControllerTest.java
@@ -1,15 +1,15 @@
 package vn.com.vndirect.web;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class CustomerControllerTest {
     private CustomerController controller;
@@ -21,9 +21,26 @@ public class CustomerControllerTest {
 
     @Test
     public void shouldShowLoginForm() throws Exception {
-        MockMvc mockMvc = standaloneSetup(controller).build();
+        MockMvc mockMvc = standaloneSetup(controller)
+                .setViewResolvers(new StandaloneMvcTestViewResolver())
+                .build();
         mockMvc.perform(get("/customer/login"))
                 .andExpect(view().name("login"));
+    }
+
+    class StandaloneMvcTestViewResolver extends InternalResourceViewResolver {
+
+        public StandaloneMvcTestViewResolver() {
+            super();
+        }
+
+        @Override
+        protected AbstractUrlBasedView buildView(String viewName) throws Exception {
+            final InternalResourceView view = (InternalResourceView) super.buildView(viewName);
+            // prevent checking for circular view paths
+            view.setPreventDispatchLoop(false);
+            return view;
+        }
     }
 
 }
