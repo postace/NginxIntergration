4425290f5403c7a1d4e47a57174eec19674daff0
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-01-15 10:41:40 +0700

    Snapshot v0.2 for compare performance nginx - java validation

--- src/main/java/vn/com/vndirect/customer/CustomerController.java
+++ src/main/java/vn/com/vndirect/customer/CustomerController.java
@@ -1,14 +1,24 @@
 package vn.com.vndirect.customer;
 
+import org.apache.log4j.Level;
+import org.aspectj.apache.bcel.util.ClassLoaderRepository;
+import org.jose4j.jwt.consumer.InvalidJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.com.vndirect.auth.JWTParser;
+import vn.com.vndirect.auth.TokenConsumer;
 import vn.com.vndirect.auth.TokenProducer;
+import vn.com.vndirect.auth.UtilMaker;
+import vn.com.vndirect.util.LogUtil;
+
+import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -32,7 +42,7 @@ public class CustomerController {
 
     @RequestMapping(value = "/login", method = POST)
     public ResponseEntity<String> processLogin(
-                @RequestBody final Customer c) {
+            @RequestBody final Customer c) {
 
         Customer customer = customers.findByUsername(c.getUsername());
         HttpHeaders responseHeader = new HttpHeaders();
@@ -41,11 +51,31 @@ public class CustomerController {
             return new ResponseEntity<String>("Incorrect username or password", responseHeader, HttpStatus.NOT_ACCEPTABLE);
         }
 
-        TokenProducer producer = new TokenProducer("vndirect", customer.getUsername(), new String[] {"audience"}, 480f, 2f);
+        TokenProducer producer = new TokenProducer("vndirect", customer.getUsername(), new String[]{"audience"}, 480f, 2f);
         String token = producer.token(customer);
         responseHeader.add("X-AUTH-TOKEN", token);
 
         return new ResponseEntity<String>("{\"token\": " + token + "}", responseHeader, HttpStatus.OK);
+    }
+
+    // TODO: Remove method below to force nginx validate tokens
+    @RequestMapping(value = "/token-java", method = GET)
+    public Map<String, Object> checkTokenValidation(
+            @RequestHeader HttpHeaders headers) {
+
+        String authHeader = headers.getFirst("Authorization");
+        String[] splited = authHeader.split("Bearer ");
+        String token = splited[1];
+
+        JWTParser parser = new JWTParser("vndirect", UtilMaker.getSecretKey());
+        Map<String, Object> claims = null;
+        try {
+            claims = parser.parseToken(token);
+            return claims;
+        } catch (InvalidJwtException e) {
+            LogUtil.log(Level.WARN, this, e.getMessage());
+            return claims;
+        }
     }
 
 }
