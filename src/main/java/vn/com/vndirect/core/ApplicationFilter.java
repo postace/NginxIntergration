013493739abaf48cea42e03db5bcb5c799b612c3
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-01-18 16:14:25 +0700

    Adding log in filter

--- src/main/java/vn/com/vndirect/core/ApplicationFilter.java
+++ src/main/java/vn/com/vndirect/core/ApplicationFilter.java
@@ -1,5 +1,6 @@
 package vn.com.vndirect.core;
 
+import org.apache.log4j.Level;
 import org.jose4j.jwt.consumer.InvalidJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 import vn.com.vndirect.user.TokenParser;
 import vn.com.vndirect.user.UserAuthentication;
-import vn.com.vndirect.user.UserService;
+import vn.com.vndirect.util.LogUtil;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -32,12 +33,12 @@ public class ApplicationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
-        saveSessionToThreadLocal(request);
+        saveRequestToThreadLocal(request);
         chain.doFilter(req, res);
         SecurityContextHolder.getContext().setAuthentication(null);
     }
 
-    private void saveSessionToThreadLocal(HttpServletRequest request) {
+    private void saveRequestToThreadLocal(HttpServletRequest request) {
         String header = request.getHeader(AUTH_HEADER_NAME);
         if (null != header) {
             String[] headers = request.getHeader(AUTH_HEADER_NAME).split(HEADER_BEARER);
@@ -46,6 +47,10 @@ public class ApplicationFilter extends GenericFilterBean {
                 UserAuthentication userAuth
                         = (UserAuthentication) parser.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(userAuth);
+                LogUtil.log(Level.INFO, this, "Customer %s is %s at %s",
+                        userAuth.getUser().getUsername(),
+                        request.getMethod(),
+                        request.getRequestURI());
             } catch (InvalidJwtException e) {
                 e.printStackTrace();
             }
